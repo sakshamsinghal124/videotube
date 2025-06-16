3c90a624e15cab111a0a6fb32080204be9442992
Saksham singhal <sakshamsinghal811@gmail.com>
2025-06-17 00:48:00 +0530

    Created multer middleware ,also created cloudinary util to upload the files on cloudinary , also hashed the password using bcrypt , and generated access and refresh token using JWT

--- src/app.js
+++ src/app.js
@@ -1,29 +1,26 @@
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
-
 app.use(
-    cors({
-       origin: process.env.CORS_ORIGIN ,
-       credentials: true 
-    })
-)
-// common middleware 
-app.use(express.json({limit: "16kb"}))
-app.use(express.urlencoded({extended: true , limit: "16kb"}))
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+// common middleware
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 
-app.use(express.static("public"))
+app.use(express.static("public"));
 
-// import routes 
- import healthcheckRouter from "./routes/healthcheck.routes.js"
-
+// import routes
+import healthcheckRouter from "./routes/healthcheck.routes.js";
 
 // routes
 
-app.use("/api/v1/healthcheck", healthcheckRouter)
+app.use("/api/v1/healthcheck", healthcheckRouter);
 
 export { app };
-
-
---
+++ src/utilss/cloudinary.js
@@ -0,0 +1,25 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.cloud_name,
+  api_key: process.env.api_key,
+  api_secret: process.env.api_secret,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    console.log("File uploaded , File src: " + response.url);
+    fs.unlinkSync(localFilePath);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath);
+    return null;
+  }
+};
+
+export { uploadOnCloudinary };
