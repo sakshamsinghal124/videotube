35621d6a587bd6c7502fcc766c131a68ed92f47b
Saksham singhal <sakshamsinghal811@gmail.com>
2025-08-05 02:30:57 +0530

    Add user authentication, registration, profile, and channel routes and also added  user channel and watch history aggregation endpoints

--- src/controllers/usercontroller.js
+++ src/controllers/usercontroller.js
@@ -8,6 +8,7 @@ import {
 import { ApiResponse } from "../utilss/ApiResponse.js";
 import { application } from "express";
 import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
 
 const generateAccessAndrefreshToken = async (userId) => {
   try {
@@ -353,12 +354,92 @@ const getUserChanel = asynchandler(async (req, res) => {
     {
       $addFields: {
         subscribersCount: { $size: "$subscribers" },
-        channelSubscribedToCount: {
+        channelsSubscribedToCount: {
           $size: "$subscribedTo",
+        },
+        isSubscribed: {
+          $cond: {
+            if: {
+              $in: [req.user?._id, "$subscribers.subscriber"],
+            },
+            then: true,
+            else: false,
+          },
         },
       },
     },
+    {
+      $project: {
+        fullname: 1,
+        username: 1,
+        avatar: 1,
+        coverImage: 1,
+        subscribersCount: 1,
+        channelsSubscribedToCount: 1,
+        isSubscribed: 1,
+      },
+    },
   ]);
+
+  if (!chanel?.length) {
+    throw new ApiError(404, "Channel not found");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, chanel[0], "Channel fetched successfully"));
+});
+
+const getWatchHistory = asynchandler(async (req, res) => {
+  const user = await User.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(req.user._id),
+      },
+    },
+    {
+      $lookup: {
+        from: "videos",
+        localField: "watchHistory",
+        foreignField: "_id",
+        as: "watchHistoryVideos",
+        pipeline: [
+          {
+            $lookup: {
+              from: "users",
+              localField: "owner",
+              foreignField: "_id",
+              as: "ownerDetails",
+              pipeline: [
+                {
+                  $project: {
+                    fullname: 1,
+                    username: 1,
+                    avatar: 1,
+                  },
+                },
+              ],
+            },
+          },
+          {
+            $addFields: {
+              $first: "$owner",
+            },
+          },
+        ],
+      },
+    },
+  ]);
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        user[0]?.watchHistoryVideos || [],
+        "Watch history fetched successfully"
+      )
+    );
 });
 
 export {
@@ -371,4 +452,6 @@ export {
   changeCurrentPassword,
   getCurrentUser,
   updateAccountDetails,
+  getUserChanel,
+  getWatchHistory,
 };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,16 @@
 import { Router } from "express";
 
-import { registerUser, logoutUser } from "../controllers/usercontroller.js";
+import {
+  registerUser,
+  logoutUser,
+  loginUser,
+  refreshAccessToken,
+  changeCurrentPassword,
+  getCurrentUser,
+  getUserChanel,
+  updateAccountDetails,
+  updateUserAvatar,
+} from "../controllers/usercontroller.js";
 import { verifyJwt } from "../middleware/auth.middleware.js";
 import { upload } from "../middleware/multer.middleware.js";
 
@@ -24,5 +34,27 @@ router.route("/register").post(
 //secured routes
 
 router.route("/logout").post(verifyJwt, logoutUser);
+
+router.route("/change-password").post(verifyJwt, changeCurrentPassword);
+
+router.route("/current-user").get(verifyJwt, getCurrentUser);
+
+router.route("/c/:username").get(verifyJwt, getUserChanel);
+
+router.route("/update-account").patch(verifyJwt, updateAccountDetails);
+
+router
+  .route("/update-account/cover-image")
+  .patch(verifyJwt, upload.single("coverImage"), updateUserCoverImage);
+
+router
+  .route("/update-account/avatar")
+  .patch(verifyJwt, upload.single("avatar"), updateUserAvatar);
+
+router.route("/watch-history").get(verifyJwt, getWatchHistory);
+
+//unsecured routes
+router.route("/login").post(loginUser);
+router.route("/refresh-token").post(refreshAccessToken);
 
 export default router;
